6e88935d65c938faea935dfb8531be5901ced443
mmusasabi <mmusasabi@gmail.com>
2019-10-19 17:13:24 +0900

    feat layoutを追加

---
+++ app/layouts/simple.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="simple">
+    <div class="title">
+      シンプルだよ
+    </div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+.simple {
+  background: yellowgreen;
+}
+
+.title {
+  font-size: 14px;
+}
+</style>
+
--- app/pages/hoge.vue
+++ app/pages/hoge.vue
@@ -13,6 +13,7 @@
 
 <script>
 export default {
+  layout: 'simple'
 }
 </script>
 
